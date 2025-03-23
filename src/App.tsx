7a02e949bd059528aba625217ddc00ab3bbc81a6
penghui <penghui.zhang@foxmail.com>
2025-03-23 17:04:22 +0800

    feat: basic canvas setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useLayoutEffect, useRef, useState } from 'react'
 import './App.css'
 
+type Element = {
+  type: 'square',
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const getCanvas = () => {
+    if (!canvasRef.current) {
+      throw new Error('canvas not found')
+    }
+    return canvasRef.current
+  }
+
+  const contextRef = useRef<CanvasRenderingContext2D>(null);
+  const getContext = () => {
+    if (!contextRef.current) {
+      throw new Error('context not found')
+    }
+
+    return contextRef.current!
+  }
+
+  const [elements, setElements] = useState<Element[]>(() => {
+    const elements = [
+      {
+        type: "square",
+        x: 100,
+        y: 100,
+        width: 100,
+        height: 100,
+      }
+    ]
+    return elements as Element[];
+  })
+
+  const paint = useCallback(() => {
+    const context = getContext();
+    elements.forEach(element => {
+      if (element.type === 'square') {
+        context.fillRect(element.x, element.y, element.width, element.height)
+      }
+    })
+  }, [elements])
+
+  useLayoutEffect(() => {
+    const canvas = getCanvas();
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    contextRef.current = canvas.getContext('2d');
+    const context = getContext();
+  })
+
+  useLayoutEffect(() => {
+    paint();
+  }, [paint])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <canvas
+      ref={canvasRef}
+      style={{
+        width: 600,
+        height: 600,
+        border: '1px solid black',
+      }}></canvas>
   )
 }
 
